840be716022c944d96092c82ce2a237a486086a9
Anthony Martinez <Martinezanthony213@gmail.com>
2019-11-13 20:09:47 -0800

    App Demo
    Car Culture Demo app now works and functional on toggle.

---
+++ js/appdemo.js
@@ -0,0 +1,8 @@
+function showDemo() {
+    var x = document.getElementById("demoDiv");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
