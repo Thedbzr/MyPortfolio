b1f2e3a25c36e4727dfb858f990e3e5a5ef8547b
Anthony Martinez <Martinezanthony213@gmail.com>
2019-12-04 16:40:03 -0800

    video button Fix

--- js/appdemo.js
+++ js/appdemo.js
@@ -6,3 +6,10 @@ function showDemo() {
       x.style.display = "block";
     }
   }
+  //use .one to ensure this only happens once
+  $("#playvideo").one(function () {
+    //as noted in addendum, check for querystring exitence
+    var symbol = $("#video1")[0].src.indexOf("?") > -1 ? "&" : "?";
+    //modify source to autoplay and start video
+    $("#video1")[0].src += symbol + "autoplay=1";
+  });
