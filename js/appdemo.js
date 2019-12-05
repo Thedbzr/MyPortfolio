1a5b419b7e4016ba2d0d34cadc63292d9a42203e
Anthony Martinez <Martinezanthony213@gmail.com>
2019-12-04 17:04:06 -0800

    Toggle Appdemo Removed

--- js/appdemo.js
+++
@@ -1,8 +0,0 @@
-/* function showDemo() {
-    var x = document.getElementById("demoDiv");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  } */
