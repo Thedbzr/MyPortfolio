03401f30e7eb3f4be4452630a5e5ad708d76da24
Anthony Martinez <Martinezanthony213@gmail.com>
2019-12-04 17:17:53 -0800

    Toggle Video

--- js/appdemo.js
+++ js/appdemo.js
@@ -1,8 +1,8 @@
-/* function showDemo() {
+function showDemo() {
     var x = document.getElementById("demoDiv");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
-  } */
+  }
